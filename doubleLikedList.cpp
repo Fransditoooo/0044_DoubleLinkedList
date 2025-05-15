726313726ef9414d11f78f974e31e5cd0ce4d377
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-05-15 11:01:19 +0700

    add step 8= insert om between node

--- doubleLikedList.cpp
+++ doubleLikedList.cpp
@@ -53,9 +53,29 @@ class DoubleLinkedList
                 // Step 6
                 newNode->prev = NULL;
 
-                
+                // Step 7
+                START = newNode;
+                return;
                 
             }
+
+            //insert om between node
+            //step 8
+            Node *current = START;
+            while (current->next !=NULL && current->next->noMhs)
+            {
+                current = current->next;
+            }
+
+            if (current->next !=NULL && nim == current->next->noMhs)
+            {
+                cout << "\nDuplicate roll number not allowed" << endl;
+                return;
+            }
+
+            
+
+        
 
 
         }
